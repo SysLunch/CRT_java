e65e5b8e5d76de17a4b52571d8b1726c79c29193
João Paulo Polles <joaopaulopolles@gmail.com>
2016-09-23 12:42:17 -0300

    Alguns problemas relativos a tipagem de argumentos, e quais argumentos do retorno que devem ser utilizados alterados.

--- src/main/java/xyz/syslunch/crt/view/DefaultView.java
+++ src/main/java/xyz/syslunch/crt/view/DefaultView.java
@@ -369,7 +369,7 @@ public class DefaultView extends JFrame {
 						
 					}else{
 						// BOM ALMOÇO TICKET
-						panel_successTicket.setTipoTicket(jo.getInt("tipo"));
+						panel_successTicket.setTipoTicket(jo.getString("texto"));
 						cl.show(panelCardlayout, "successTicket");
 						aBomAlmoco = new Thread(audioBomAlmoco);
 						aBomAlmoco.start();
